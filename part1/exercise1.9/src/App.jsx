19b1f9413b5c50ba8aed0d6773ed18327d9f37f8
Chimix1 <85110200+Chimix1@users.noreply.github.com>
2024-01-06 14:32:09 +0200

    1.92

--- part1/exercise1.9/src/App.jsx
+++ part1/exercise1.9/src/App.jsx
@@ -1,10 +1,54 @@
 import { useState } from 'react'
 
+const Statistics = (props) => {
+  if (props.total === 0) {
+		return (
+			<div>
+				<h3>No Feedback Given Now</h3>
+			</div>
+		)
+	} 
+	return (
+		<div>
+			<p>Good: {props.good}</p>
+			<p>Neutral: {props.neutral}</p>
+			<p>Bad: {props.bad}</p>
+			<p>All: {props.total}</p>
+			<p>Average: {props.Average}</p>
+			<p>Positive: {props.Positive} %</p>
+		</div>
+	)
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  //const [allClicks, setAll] = useState([])
+  //const [total, setTotal] = useState(0)
+
+  //const handleGoodClick = () => {
+    //setAll(allClicks.concat())
+    //const updatedGood = good + 1
+    //setGood(updatedGood)
+    //setTotal(updatedGood + neutral + bad)
+  //}
+
+  //const handleNeutralClick = () => {
+    //setAll(allClicks.concat())
+    //const updatedNeutral = neutral + 1
+    //setNeutral(updatedNeutral)
+    //setTotal(good + updatedNeutral + bad)
+  //}
+
+  //const handleBadClick = () => {
+    //setAll(allClicks.concat())
+    //const updatedBad = bad + 1
+    //setBad(updatedBad)
+    //setTotal(good + neutral + updatedBad)
+  //}
+  
 
   return (
     <div>
@@ -20,9 +64,14 @@ const App = () => {
         bad
       </button>
       <p>Statistics</p>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
+      <Statistics
+      good={good}
+      neutral={neutral}
+      bad={bad}
+      total={good + neutral + bad}
+      Average={(good - bad)/(good + neutral + bad)}
+      Positive={(good + neutral)/(good + neutral + bad)*(100)} 
+      />
     </div>
   )
 }
