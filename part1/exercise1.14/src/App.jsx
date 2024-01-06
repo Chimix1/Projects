4ecb8aab43813c68742b01d9adb1b7d3598ebf93
Chimix1 <85110200+Chimix1@users.noreply.github.com>
2024-01-06 20:59:08 +0200

    1.12 1.13

--- part1/exercise1.14/src/App.jsx
+++ part1/exercise1.14/src/App.jsx
@@ -1,28 +1,37 @@
 import { useState } from 'react'
 
 const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+   
+  const [selected, setSelected] = useState(0)
+  const [vote, setVote] = useState(anecdotes.map(() => 0))
+
+  const RandomAnecdote = () => {
+      setSelected(Math.floor(Math.random() * anecdotes.length))
+  }
+
+  const Vote = () => {
+    const newVote = [...vote]
+    newVote[selected] += 1
+    setVote(newVote)
+  }
 
   return (
     <div>
-      <p>Give feedback</p>
-      <button onClick={() => setGood(good + 1)}>
-        good
-      </button>
-      
-      <button onClick={() => setNeutral(neutral + 1)}>
-        neutral
-      </button>
-      <button onClick={() => setBad(bad + 1)}>
-        bad
-      </button>
-      <p>Statistics</p>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
+      {anecdotes[selected]}
+      <p>has {vote[selected]} vote</p>
+      <button onClick={RandomAnecdote}>next anecdote</button>
+      <button onClick={Vote}>vote</button>
     </div>
   )
 }
