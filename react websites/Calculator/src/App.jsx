e0a3f4163ae8390698d4a2917bd248674eed5308
Chimix1 <85110200+Chimix1@users.noreply.github.com>
2024-11-25 20:20:39 +0200

    More updates to calculator

--- react websites/Calculator/src/App.jsx
+++ react websites/Calculator/src/App.jsx
@@ -1,11 +1,30 @@
+import { useState } from "react";
 import Header from "./components/Header.jsx"
 import Userinput from "./components/Userinput.jsx"
+import Results from "./components/Results.jsx";
 
 function App() {
+  const [userInput, setUserInput] = useState({
+    intialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10
+});
+
+function handleChange(inputIdentifier ,newValue) {
+  setUserInput(prevUserInput => {
+      return{
+          ...prevUserInput,
+          [inputIdentifier]: +newValue
+      };
+  });
+}
+
   return (
     <>
     <Header/>
-    <Userinput/>
+    <Userinput userInput={userInput} onChange={handleChange}/>
+    <Results input={userInput}/>
     </>
   )
 }
