fbdf72ab76597a34148503eabcef08d44e45cb0d
Chimix1 <85110200+Chimix1@users.noreply.github.com>
2024-11-25 20:01:49 +0200

    Calculator values

---
+++ react websites/Calculator/src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./components/Header.jsx"
+import Userinput from "./components/Userinput.jsx"
+
+function App() {
+  return (
+    <>
+    <Header/>
+    <Userinput/>
+    </>
+  )
+}
+
+export default App
