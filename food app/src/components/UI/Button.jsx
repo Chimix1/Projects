8b8135ce94e2d666b6427a257423acb4509ec3de
Chimix1 <85110200+Chimix1@users.noreply.github.com>
2025-01-06 18:51:42 +0200

    food cart project

---
+++ food app/src/components/UI/Button.jsx
@@ -0,0 +1,10 @@
+export default function Button({ children, textOnly, className, ...props }) {
+    let ccsClasses = textOnly ? 'text-button' : 'button';
+    ccsClasses += ' ' + className;
+
+    return (
+    <button className={ccsClasses} {...props}>
+        {children}
+    </button>
+    );
+}
