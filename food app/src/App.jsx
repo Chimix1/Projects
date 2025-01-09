d9d6572e2b41f5ceeb48f71108ffee13008d34e1
Chimix1 <85110200+Chimix1@users.noreply.github.com>
2025-01-09 13:51:03 +0200

    food app

--- food app/src/App.jsx
+++ food app/src/App.jsx
@@ -4,18 +4,27 @@ import Header from "./components/Header";
 import Meals from "./components/Meals";
 import { CartContextProvider } from "./store/CartContext";
 import { UserProgressContextProvider } from "./store/UserProgressContext";
+import { Opinions } from './components/Opinions';
+import { NewOpinion } from './components/NewOpinion';
+import { OpinionsContextProvider } from './store/opinions-context';
+
 
 
 function App() {
   return (
     <UserProgressContextProvider>
     <CartContextProvider>
+    <OpinionsContextProvider>
       <Header />
       <Meals />
       <Cart />
       <Checkout />
+      <NewOpinion />
+      <Opinions />
+      </OpinionsContextProvider>
     </CartContextProvider>
     </UserProgressContextProvider>
+    
   );
 }
 
